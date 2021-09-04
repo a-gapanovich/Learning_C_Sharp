a617f56a761bdc2025e36b25358431aec0d6505e
a-gapanovich <a.gapanovich.09@gmail.com>
2021-09-04 19:23:30 +0300

    first program

---
+++ Start_learn/Start_learn/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Start_learn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
