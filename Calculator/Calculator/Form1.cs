d70a78d6219e7933fa31fb8a5b3df365308bdc4d
a-gapanovich <a.gapanovich.09@gmail.com>
2021-09-04 21:34:32 +0300

    second program

---
+++ Calculator/Calculator/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float a = float.Parse(textBox1.Text);
+            float b = float.Parse(textBox2.Text);
+            float c = a + b;
+            textBox3.Text = c.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(textBox1.Text);
+            double b = double.Parse(textBox2.Text);
+            double c = a / b;
+            textBox3.Text = c.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            float a = float.Parse(textBox1.Text);
+            float b = float.Parse(textBox2.Text);
+            float c = a - b;
+            textBox3.Text = c.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            float a = float.Parse(textBox1.Text);
+            float b = float.Parse(textBox2.Text);
+            float c = a * b;
+            textBox3.Text = c.ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
