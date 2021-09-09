ce2a5637a3f4e72328c2cc7eba0d5bfa21050dd8
a-gapanovich <a.gapanovich.09@gmail.com>
2021-09-09 22:18:33 +0300

    counter

---
+++ C+Sharp_Mini_programs/C+Sharp_Mini_programs/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C_Sharp_Mini_programs
+{
+    public partial class Form1 : Form
+    {
+        int count = 0;
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void оПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Здесь может быть Ваша реклама!", "О программе.");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            count++;
+            lblCount.Text = Convert.ToString(count);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            count--;
+            lblCount.Text = Convert.ToString(count);
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnReser_Click(object sender, EventArgs e)
+        {
+            count = 0;
+            lblCount.Text = Convert.ToString(count);
+        }
+    }
+}
