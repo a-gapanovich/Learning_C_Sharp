c773af0e54cc8865304411b1ae05d49d52b078f5
a-gapanovich <a.gapanovich.09@gmail.com>
2021-09-14 22:21:10 +0300

    fist_conect

---
+++ Com_port/Com_port/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Com_port
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.IO.Ports.SerialPort sport = new System.IO.Ports.SerialPort(maskedTextBox1.Text, 9600, System.IO.Ports.Parity.None, 8, System.IO.Ports.StopBits.One);
+            try
+            {
+                sport.Open();
+                sport.Write("Test");
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(Convert.ToString(ex));
+            }
+        }
+    }
+}
